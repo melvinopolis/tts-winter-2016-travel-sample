5e38d6fcc4d759e2c2edabd970d682213b9e4344
Mariel Van Norman <melvinopolis@gmail.com>
2016-02-24 18:41:42 -0500

    scaffold destinations resource and add root route

---
+++ app/views/destinations/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @destination, :id, :city, :country, :address, :description, :created_at, :updated_at
---
+++ db/migrate/20160224234025_create_destinations.rb
@@ -0,0 +1,12 @@
+class CreateDestinations < ActiveRecord::Migration
+  def change
+    create_table :destinations do |t|
+      t.string :city
+      t.string :country
+      t.string :address
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
